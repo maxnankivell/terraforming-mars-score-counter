4e4ace4b7bd34e77b4b0e7108f0a680fec04e78f
Eva-Sorensen <88927232+Eva-Sorensen@users.noreply.github.com>
2023-02-19 20:57:50 +1300

    Modals added (#3)
    Co-authored-by: maxnankivell <maxnankivell1@gmail.com>

---
+++ src/lib/ConfirmModal.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	import BaseModal from './BaseModal.svelte';
+
+	export let text: string;
+
+	const dispatch = createEventDispatcher();
+	const close = () => dispatch('close');
+	const confirm = () => dispatch('confirm');
+
+	const onClose = () => {
+		close();
+	};
+	const onConfirm = () => {
+		confirm();
+	};
+</script>
+
+<BaseModal on:close={onClose}>
+	<div slot="header" class="flex flex-col items-center mb-2">
+		<h2 class="text-xl text-center"><b>{text}</b></h2>
+	</div>
+
+	<div slot="footer" class="flex items-center justify-center gap-5">
+		<button
+			class="button-base text-white bg-cadmiumOrange-500 hover:bg-cadmiumOrange-400"
+			on:click={onClose}
+		>
+			Cancel
+		</button>
+		<button class="button-base bg-champagne-500 hover:bg-champagne-400" on:click={onConfirm}>
+			Confirm
+		</button>
+	</div>
+</BaseModal>
+
+<style lang="postcss">
+</style>
