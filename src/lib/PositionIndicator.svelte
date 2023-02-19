dece243c956c2f439482297c9b76bed9e8de3688
Eva Sorensen <eva.elaine.sorensen@gmail.com>
2023-02-19 21:28:14 +1300

    remove if stament for turnary

--- src/lib/PositionIndicator.svelte
+++ src/lib/PositionIndicator.svelte
@@ -5,11 +5,7 @@
 
 <div class="flex flex-row gap-3">
 {#each Array(numberDots) as _, i}
-    {#if i === currentPosition}
-        <div class='bg-cadmiumOrange rounded-full w-1.5 h-1.5'>
-        </div>
-    {/if}
-    <div class='bg-champagne rounded-full w-1.5 h-1.5'></div>
+    <div class='{i === currentPosition ? 'bg-cadmiumOrange' : 'bg-champagne'} rounded-full w-1.5 h-1.5' />
 {/each}
 </div>
 
